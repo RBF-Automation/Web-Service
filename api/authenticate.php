63af9af4345a82e58d4ef27517493ef888f0b537
Brian Floersch <gh123man@gmail.com>
2015-02-01 14:46:32 -0500

    added some error handling

--- api/authenticate.php
+++ api/authenticate.php
@@ -8,11 +8,14 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = strtolower($_POST['username']);
     $password = $_POST['password'];
 
-    $account = Account::login($username, $password);
-    if ($account != false) {
-        $out = array('result' => true, "message" => 'login successful', 'token' => $account->authToken());
-    } else {
-        session_destroy();
+    try {
+        $account = Account::login($username, $password);
+        if ($account != false) {
+            $out = array('result' => true, "message" => 'login successful', 'token' => $account->authToken());
+        } else {
+            $out = array('result' => false, "message" => 'failed to login');
+        }
+    } catch (Exception $e) {
         $out = array('result' => false, "message" => 'failed to login');
     }
 
--- api/startSession.php
+++ api/startSession.php
@@ -5,13 +5,17 @@ include_once '../src/Account.php';
 
 if (isset($_POST['token'])) {
     session_start();
-    $account = Account::fromToken($_POST['token']);
-    if ($account->getID() != null) {
-        $_SESSION['userid'] = $account->ID();
-        $out = array('result' => true, "message" => 'session stared');
-    } else {
-        session_destroy();
-        $out = array('result' => false, "message" => 'failed to start session');
+    try {
+        $account = Account::fromToken($_POST['token']);
+        if ($account->getID() != null) {
+            $_SESSION['userid'] = $account->ID();
+            $out = array('result' => true, "message" => 'session stared');
+        } else {
+            session_destroy();
+            $out = array('result' => false, "message" => 'failed to start session');
+        }
+     } catch (Exception $e) {
+        $out = array('result' => false, "message" => 'failed to login');
     }
 
 } else {
