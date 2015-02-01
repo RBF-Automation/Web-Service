c29ae41c84ee85b5814065f75f2059a1a3728878
Brian Floersch <gh123man@gmail.com>
2015-02-01 14:50:45 -0500

    added test manifest

--- api/getManifest.php
+++ api/getManifest.php
@@ -6,6 +6,15 @@ include_once '../accountUtils.php';
 session_start();
 if (checkLogin()) {
     error_log('loggedin! ID:' . $_SESSION['userid']);
+    $testManifest = array(
+        "id" => 1,
+        "name" => "Front Door",
+        "btn_on" => "Unlock",
+        "btn_off" => "Lock",
+    );
+    $out = array();
+    $out[] = $testManifest;
+    echo json_encode($out);
 } else {
     error_log('not logged in');
 }
