15b0d6817ef57e8a3a24fd5be740850877b903de
Brian Floersch <gh123man@gmail.com>
2015-02-17 15:53:47 -0500

    more style and formatting updates

--- conf/index.php
+++ conf/index.php
@@ -24,12 +24,12 @@ if (!checkLogin()) {
     }
     .node {
         margin: 10px;
-        padding: 5px;
+        padding: 8px;
         float: left;
     }
     .account {
         margin: 10px;
-        padding: 5px;
+        padding: 8px;
         float: left;
     }
     .topBar {
@@ -67,8 +67,20 @@ if (!checkLogin()) {
     .hover:hover {
         background: #F0F0F0;
     }
+    .indentContent {
+        margin: 20px;
+        
+    }
+    .createNode {
+        margin: 10px;
+        padding-top: 15px;
+        padding-left: 10px;
+        padding-right: 10px;
+        float: left;
+    }
     tr, td {
         padding: 2px;
+        padding-right: 5px;
         border: 0px;
     }
     body {
@@ -76,7 +88,10 @@ if (!checkLogin()) {
         background: #E4E4E4;
         margin-top: 70px;
     }
-    table {border: none;}
+    table {
+        border: none;
+        margin: 0 auto;
+    }
     </style>
 </head>
 
@@ -86,150 +101,191 @@ if (!checkLogin()) {
 </div>
 
 <h2> Accounts </h2>
-<h3> Create Account </h3>
-<form action="createAccount.php" method="post">
-    <div class="account card">
-        <table>
-            <tr>
-                <td>username</td>
-                <td><input name="username"/></td>
-            </tr>
-            <tr>
-                <td>password</td>
-                <td><input type="password" name="password"/></td>
-            </tr>
-            <tr>
-                <td><input type="submit"/></td>
-            </tr>
-        </table>
-    </div>
-</form>
+<div class="indentContent">
+    <h3> Create Account </h3>
+    <form action="createAccount.php" method="post">
+        <div class="account card">
+            <table>
+                <tr>
+                    <td>username</td>
+                    <td><input name="username"/></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" name="password"/></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input style="float: right;" type="submit"/></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+    <br style="clear: both" />
+
+    <h3> Manage Accounts </h3>
+    <?php
+    foreach (Account::getAccounts() as $account) {
+        ?>
+        <div class="account card">
+            <table>
+                <tr>
+                    <td>username</td>
+                    <td><span style='font-weight: bold;'><?= $account->username(); ?></span></td>
+                </tr>
+                <tr>
+                    <td>Created on</td>
+                    <td><span style='font-weight: bold;'><?= date('j/n/Y h:i:s A', $account->time()); ?></span></td>
+                </tr>
+                <tr>
+                    <td><a href="deleteAccount.php?id=<?= $account->ID(); ?>"> delete Account</a></td>
+                    <td><a href="deauth.php?id=<?= $account->ID(); ?>"> reset token</a></td>
+                </tr>
+            </table>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+
 <br style="clear: both" />
 
-<h3> Manage Accounts </h3>
-<?php
-foreach (Account::getAccounts() as $account) {
-    ?>
-    <div class="account card">
-        <table>
-            <tr>
-                <td>username</td>
-                <td><span style='font-weight: bold;'><?= $account->username(); ?></span></td>
-            </tr>
-            <tr>
-                <td>Account created on</td>
-                <td><span style='font-weight: bold;'><?= date('j/n/Y h:i:s A', $account->time()); ?></span></td>
-            </tr>
-            <tr>
-                <td><a href="deleteAccount.php?id=<?= $account->ID(); ?>"> delete Account</a></td>
-                <td><a href="deauth.php?id=<?= $account->ID(); ?>"> reset token</a></td>
-            </tr>
-        </table>
-    </div>
-    <?php
-}
-?>
-<br style="clear: both" />
 <h2> Nodes </h2>
-<h3> Create Node </h3>
-<form action="createNode.php" method="post">
-<select name="nodeType">
-<?php
-foreach (NodeTypes::$map as $key => $value) {
-    echo '<option value="' . $key . '">' . $value . '</option>';
-}
-?>
-</select>
-<input type="submit"/>
-</form>
-
-<h3> Manage Nodes </h3>
-<?php
-foreach (Node::getNodes() as $node) {
-    $type = $node->type();
-    ?>
-    <div class="node card">
-    <table>
-        <tr>
-            <td>nodeType</td>
-            <td><span style='font-weight: bold;'><?= NodeTypes::$map[$type]; ?></span></td>
-        </tr>
-        <tr>
-            <td>Node created on</td>
-            <td><span style='font-weight: bold;'><?= date('j/n/Y h:i:s A', $node->time()); ?></span></td>
-        </tr>
-        <tr>
-            <td><a href="deleteNode.php?id=<?= $node->ID(); ?>"> delete Node</a></td>
-        </tr>
-    </table>
+<div class="indentContent">
+    <h3> Create Node </h3>
+    <div class="createNode card">
+        <form action="createNode.php" method="post">
+        <select name="nodeType">
+        <?php
+        foreach (NodeTypes::$map as $key => $value) {
+            echo '<option value="' . $key . '">' . $value . '</option>';
+        }
+        ?>
+        </select>
+        <input type="submit"/>
+        </form>
+    </div>
+    
+    <br style="clear: both" />
+    
+    <h3> Manage Nodes </h3>
     <?php
+    foreach (Node::getNodes() as $node) {
+        $type = $node->type();
+        ?>
+        <div class="node card">
+        <table>
+            <tr>
+                <td>Type</td>
+                <td><span style='font-weight: bold;'><?= NodeTypes::$map[$type]; ?></span></td>
+            </tr>
+            <tr>
+                <td>Created on</td>
+                <td><span style='font-weight: bold;'><?= date('j/n/Y h:i:s A', $node->time()); ?></span></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><a style="float: right;" href="deleteNode.php?id=<?= $node->ID(); ?>"> delete Node</a></td>
+            </tr>
+        </table>
+        <?php
 
-    switch ($node->type()) {
-        case NodeTypes::SWITCH_NODE:
-            $props = new SwitchNodeProperties($node->ID());
-            ?>
-            <div style="display: block;">
-                <form action="editSwitchNode.php?ID=<?= $node->ID(); ?>" method="post">
+        switch ($node->type()) {
+            case NodeTypes::SWITCH_NODE:
+                $props = new SwitchNodeProperties($node->ID());
+                ?>
+                <div style="display: block; padding-top: 10px">
+                    <form action="editSwitchNode.php?ID=<?= $node->ID(); ?>" method="post">
+                        <table>
+                            <tr>
+                                <td>nodeId</td>
+                                <td><input name="nodeId" value="<?= $props->nodeId(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td>Name</td>
+                                <td><input name="name" value="<?= $props->name(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td>On text</td>
+                                <td><input name="on" value="<?= $props->btn_on(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td>Off text</td>
+                                <td><input name="off" value="<?= $props->btn_off(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td>On log message</td>
+                                <td><input name="logMessageOn" value="<?= $props->logMessageOn(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td>Off log message</td>
+                                <td><input name="logMessageOff" value="<?= $props->logMessageOff(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td><input style="float: right;" type="submit"/></td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+                
+                <?php
+                break;
+                
+            case NodeTypes::IP_TRACKER:
+                $props = new IpTrackerNodeProperties($node->ID());
+                ?>
+                <div style="display: block; padding-top: 10px">
+                    <form action="editIpTrackerNode.php?ID=<?= $node->ID(); ?>" method="post">
+                        <table>
+                            <tr>
+                                <td>server</td>
+                                <td><input name="server" value="<?= $props->server(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td>Name</td>
+                                <td><input name="name" value="<?= $props->name(); ?>"/></td>
+                            </tr>
+                            <tr>
+                                <td>newIp</td>
+                                <td>
+                                    <input name="newIp"/>
+                                </td>
+                                <td>
+                                    <select name="user">
+                                    <?php
+                                    foreach (Account::getAccounts() as $account) {
+                                        echo '<option value="' . $account->ID() . '">' . $account->username() . '</option>';
+                                    }
+                                    ?>
+                                    </select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td><input style="float: right;" type="submit"/></td>
+                            </tr>
+                        </table>
+                    </form>
                     <table>
                         <tr>
-                            <td>nodeId</td>
-                            <td><input name="nodeId" value="<?= $props->nodeId(); ?>"/></td>
-                        </tr>
-                        <tr>
-                            <td>Name</td>
-                            <td><input name="name" value="<?= $props->name(); ?>"/></td>
-                        </tr>
-                        <tr>
-                            <td>On text</td>
-                            <td><input name="on" value="<?= $props->btn_on(); ?>"/></td>
-                        </tr>
-                        <tr>
-                            <td>Off text</td>
-                            <td><input name="off" value="<?= $props->btn_off(); ?>"/></td>
-                        </tr>
-                        <tr>
-                            <td>On log message</td>
-                            <td><input name="logMessageOn" value="<?= $props->logMessageOn(); ?>"/></td>
-                        </tr>
-                        <tr>
-                            <td>Off log message</td>
-                            <td><input name="logMessageOff" value="<?= $props->logMessageOff(); ?>"/></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit"/></td>
+                            <?php
+                            foreach ($props->getUsers() as $data) {
+                                $user = new Account($data['user']);
+                                echo '<tr><td>' . $data['ip'] . '</td>' . $user->username() . ' ' . '<td><a href="editIpTrackerNode.php?ID=' . $node->ID() . '&delete=' . $data['ip'] . '">delete</a></td></tr>';
+                            }
+                            ?>
                         </tr>
                     </table>
-                </form>
-            </div>
-            
-            <?php
-            break;
-            
-        case NodeTypes::IP_TRACKER:
-            $props = new IpTrackerNodeProperties($node->ID());
-            echo '<div style="margin-left: 40px; display: block;">';
-            echo '<form action="editIpTrackerNode.php?ID=' . $node->ID() . '" method="post">';
-            echo 'server<input name="server" value="' . $props->server() . '"/><br/>';
-            echo 'Name<input name="name" value="' . $props->name() . '"/><br/>';
-            echo 'newIp<input name="newIp"/><select name="user">';
-            foreach (Account::getAccounts() as $account) {
-                echo '<option value="' . $account->ID() . '">' . $account->username() . '</option>';
-            }
-            echo '</select><br/>';
-            echo '<input type="submit"/>';
-            echo '</form>';
-            foreach ($props->getUsers() as $data) {
-                $user = new Account($data['user']);
-                echo $data['ip'] . ' ' . $user->username() . ' ' . ' <a href="editIpTrackerNode.php?ID=' . $node->ID() . '&delete=' . $data['ip'] . '">delete</a>';
-                echo '<br/>';
-            }
-            echo '</div>';
-            
-            break;
+                </div>
+                <?php
+                break;
+
+        }
+        echo '</div>';
+
 
     }
-    echo '</div>';
-
-
-}
-?>
+    ?>
+</div>
